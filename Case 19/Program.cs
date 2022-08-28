5dad6b0ec60ef80229ec6d4089743289dd0b4f76
Margarita <bano4kaa@mail.ru>
2022-08-28 03:04:09 +0300

    case 19 done

--- Case 19/Program.cs
+++ Case 19/Program.cs
@@ -3,3 +3,25 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+void Reverse()
+{
+Console.WriteLine("Введите целое пятизначное число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int a = 10000;
+int y = N;
+int result = 0;
+while (i < 5) 
+{
+result += y%10 * a;
+a = a/10;
+y = y/10;
+i++;
+}
+if (result == N)
+{
+Console.WriteLine("Да, введённое число является палиндромом");
+}
+else Console.WriteLine("Нет, введённое число не является палиндромом ");
+}
+Reverse();
